1ad60c4aafb0422acaf0b2de2a6dd994dd46b1b0
Brenton McSweyn <brenton.mcsweyn@gmail.com>
2019-06-06 22:01:48 +1000

    Adding Matrices - Multiply and Transpose

---
+++ src/raytracer/Matrix.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Linq;
+using rayTracer.Helpers;
+
+namespace rayTracer
+{
+    public class Matrix
+    {
+        private readonly int _rows;
+        private readonly int _columns;
+        private readonly float[] _values;
+
+        public static Matrix Identity = new Matrix(4, 4, new[] {1f, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1});
+
+        public Matrix(int rows, int columns, float[] values)
+        {
+            _rows = rows;
+            _columns = columns;
+            _values = values;
+
+            var requiredValues = _columns * rows;
+            if (_values.Length != requiredValues)
+                throw new NotSupportedException("There needs to be " + requiredValues + " values in the array");
+        }
+
+        private Matrix(int rows, int columns)
+        {
+            _rows = rows;
+            _columns = columns;
+
+            var requiredValues = rows * columns;
+            _values = new float[requiredValues];
+
+            for (var i = 0; i < requiredValues; i++)
+            {
+                _values[i] = 0f;
+            }
+        }
+
+        public static Matrix operator *(Matrix mA, Matrix mB)
+        {
+            var mR = new Matrix(mA._rows, mA._rows);
+            for (var r = 0; r < mA._rows; r++)
+            {
+                for (var c = 0; c < mA._columns; c++)
+                {
+                    mR[r, c] = MultiplyRowCol(mA, mB, r, c);
+                }
+            }
+
+            return mR;
+        }
+
+        public static Tuple operator *(Matrix mA, Tuple t)
+        {
+            var mT = new Matrix(4, 1, new[] {t.X, t.Y, t.Z, t.W});
+
+            var x = MultiplyRowCol(mA, mT, 0, 0);
+            var y = MultiplyRowCol(mA, mT, 1, 0);
+            var z = MultiplyRowCol(mA, mT, 2, 0);
+            var w = MultiplyRowCol(mA, mT, 3, 0);
+
+            return new Tuple(x, y, z, w);
+        }
+
+        private static float MultiplyRowCol(Matrix mA, Matrix mB, int r, int c)
+        {
+            return mA[r, 0] * mB[0, c] +
+                   mA[r, 1] * mB[1, c] +
+                   mA[r, 2] * mB[2, c] +
+                   mA[r, 3] * mB[3, c];
+        }
+
+        public float this[int row, int col]
+        {
+            get => _values[GetIndex(row, col)];
+            private set => _values[GetIndex(row, col)] = value;
+        }
+
+        public Matrix Transpose()
+        {
+            var result = new float[_rows * _columns];
+            var resultIndex = 0;
+
+            for (var c = 0; c < _columns; c++)
+            for (var r = 0; r < _rows; r++)
+            {
+                result[resultIndex] = _values[r * _rows + c];
+                resultIndex++;
+            }
+
+            return new Matrix(_rows, _columns, result);
+        }
+
+        private int GetIndex(int row, int col) => row * _columns + col;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Matrix) obj);
+        }
+
+        private bool Equals(Matrix other)
+        {
+            if (_values.Length != other._values.Length)
+                return false;
+
+            for (var i = 0; i < _values.Length; i++)
+            {
+                if (!_values[i].AlmostEqual(other._values[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return _values != null ? _values.GetHashCode() : 0;
+        }
+
+        public override string ToString()
+        {
+            return _values.Aggregate(string.Empty, (current, v) => current + (v + " "));
+        }
+    }
+}
---
+++ src/xUnit/MatrixTests.cs
@@ -0,0 +1,129 @@
+using Xunit;
+
+namespace rayTracer.xUnit
+{
+    public class MatrixTests
+    {
+        [Fact]
+        public void ConstructAndInspectMatrix4X4()
+        {
+            var m = new Matrix(4, 4,
+                new[] {1, 2, 3, 4, 5.5f, 6.5f, 7.5f, 8.5f, 9, 10, 11, 12, 13.5f, 14.5f, 15.5f, 16.5f});
+
+            Assert.Equal(1, m[0, 0]);
+            Assert.Equal(4, m[0, 3]);
+            Assert.Equal(5.5, m[1, 0]);
+            Assert.Equal(7.5, m[1, 2]);
+            Assert.Equal(11, m[2, 2]);
+            Assert.Equal(13.5, m[3, 0]);
+            Assert.Equal(15.5, m[3, 2]);
+        }
+
+        [Fact]
+        public void ConstructAndInspectMatrix2X2()
+        {
+            var m = new Matrix(2, 2,
+                new[] {-3, 5, 1, -2f});
+
+            Assert.Equal(-3, m[0, 0]);
+            Assert.Equal(5, m[0, 1]);
+            Assert.Equal(1, m[1, 0]);
+            Assert.Equal(-2, m[1, 1]);
+        }
+
+
+        [Fact]
+        public void ConstructAndInspectMatrix3X3()
+        {
+            var m = new Matrix(3, 3,
+                new[] {-3, 5, 0, 1, -2f, -7, 0, 1, 1});
+
+            Assert.Equal(-3, m[0, 0]);
+            Assert.Equal(5, m[0, 1]);
+            Assert.Equal(0, m[0, 2]);
+            Assert.Equal(1, m[1, 0]);
+            Assert.Equal(-2, m[1, 1]);
+            Assert.Equal(-7, m[1, 2]);
+
+            Assert.Equal(0, m[2, 0]);
+            Assert.Equal(1, m[2, 1]);
+            Assert.Equal(1, m[2, 2]);
+        }
+
+        [Fact]
+        public void BasicEqualityCheck()
+        {
+            var v1 = new[] {1f, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2};
+            var v2 = new[] {1f, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2};
+
+            Assert.Equal(v1, v2);
+        }
+
+        [Fact]
+        public void MatricesAreEqual()
+        {
+            var mA = new Matrix(4, 4, new[] {1f, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2});
+            var mB = new Matrix(4, 4, new[] {1f, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2});
+
+            Assert.Equal(mA, mB);
+        }
+
+        [Fact]
+        public void MatricesAreNotEqual()
+        {
+            var mA = new Matrix(4, 4, new[] {1f, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2});
+            var mB = new Matrix(4, 4, new[] {2f, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1});
+
+            Assert.NotEqual(mA, mB);
+        }
+
+        [Fact]
+        public void MatrixMultiplication()
+        {
+            var mA = new Matrix(4, 4, new[] {1f, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2});
+            var mB = new Matrix(4, 4, new[] {-2f, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8});
+
+            var mResult = new Matrix(4, 4, new[] {20f, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42});
+            Assert.Equal(mResult, mA * mB);
+        }
+
+        [Fact]
+        public void MatrixTupleMultiplication()
+        {
+            var mA = new Matrix(4, 4, new[] {1f, 2, 3, 4, 2, 4, 4, 2, 8, 6, 4, 1, 0, 0, 0, 1});
+            var t = new Tuple(1, 2, 3, 1);
+
+            Assert.Equal(new Tuple(18, 24, 33, 1), mA * t);
+        }
+
+        [Fact]
+        public void MatrixMultiplicationByIdentity()
+        {
+            var mA = new Matrix(4, 4, new[] {0f, 1, 2, 4, 1, 2, 4, 8, 2, 4, 8, 16, 4, 8, 16, 32});
+            Assert.Equal(mA, mA * Matrix.Identity);
+        }
+
+        [Fact]
+        public void MatrixTupleMultiplicationByIdentity()
+        {
+            var t = new Tuple(1, 2, 3, 4);
+
+            Assert.Equal(t, Matrix.Identity * t);
+        }
+
+        [Fact]
+        public void TransposeMatrix()
+        {
+            var mA = new Matrix(4, 4, new[] {0f, 9, 3, 0, 9, 8, 0, 8, 1, 8, 5, 3, 0, 0, 5, 8});
+            var mR = new Matrix(4,4, new []{0f,9,1,0,9,8,8,0,3,0,5,5,0,8,3,8});
+            
+            Assert.Equal(mR, mA.Transpose());
+        }
+        
+        [Fact]
+        public void TransposeMatrixIdentity()
+        {
+            Assert.Equal(Matrix.Identity, Matrix.Identity.Transpose());
+        }
+    }
+}
